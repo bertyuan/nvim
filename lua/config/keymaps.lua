23360454ee48a456c3b424dc5d4a0d3f7d6fc0f8
Bertrand Yuan <bert.yuan@outlook.com>
2025-11-21 12:27:23 +0800

    add <kbd>kk</kbd> for getting back to normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 -- keymaps
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "kk", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = false })
