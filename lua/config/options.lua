ea193e58b59ee9e48c93ce8e27215dc231a55dce
Bertrand Yuan <bert.yuan@outlook.com>
2025-11-22 01:01:36 +0800

    feat: show whilespaces and special characters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- show special characters
+vim.opt.list = true
+
+vim.opt.listchars = {
+  tab = "->",
+  space = "·",    -- space as `·`
+  trail = "·",    -- spaces in the end of line
+  extends = "▶",
+  precedes = "◀",
+  nbsp = "␣",
+}
