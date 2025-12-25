4e305140a59a53772c4afadc90b510c5ab24cf0f
Bertrand Migadu <me@migadu.dignity.team>
2025-12-25 23:37:42 +0800

    feat: use system clipboard
    Sometimes it is useful, sometimes it is not.
    After mastering vim buffer, would revert this.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,9 @@
 -- show special characters
 vim.opt.list = true
 
+-- use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.listchars = {
   tab = "-->",
   space = "·",    -- space as `·`
