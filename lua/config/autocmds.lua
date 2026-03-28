12285173b0c2732c0e68fe193c7a0cbf173ffe3d
Bertrand Yuan <me@bertyuan.com>
2026-03-28 15:15:18 +0800

    daily update
    The fact is I forgot what I have done.
    
    Signed-off-by: Bertrand Yuan <me@bertyuan.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,6 +15,20 @@ autocmd("FileType", {
   pattern = { "c", "cpp", "objc", "objcpp", "cuda", "text" },
   callback = function()
     vim.opt_local.colorcolumn = "81"
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 8
+    vim.opt_local.expandtab = false
+    -- hard wrap
     -- vim.opt_local.textwidth = 80
+  end,
+})
+
+autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[%s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, pos)
   end,
 })
--- lua/plugins/vscode-theme.lua
+++ lua/plugins/vscode-theme.lua
@@ -10,19 +10,19 @@ return {
     require("vscode").setup({
       transparent = true,
       underline_links = true,
-      -- 禁用 nvim-tree 背景颜色
       disable_nvimtree_bg = true,
-      -- 将主题颜色应用到终端
       terminal_colors = true,
-      -- 覆盖颜色（参见 lua/vscode/colors.lua）
-      color_overrides = {
-        -- vscLineNumber = "#FFFFFF",
-      },
-      -- 覆盖高亮组（参见 lua/vscode/theme.lua）
+
+      color_overrides = {},
+
       group_overrides = {
-        -- 支持与 vim.api.nvim_set_hl 相同的参数
-        -- 可以通过 require('vscode.colors') 使用主题颜色
-        -- Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+        Keyword = { bold = true },
+        ["@keyword"] = { bold = true },
+
+        ["@keyword.conditional"] = { bold = true },
+        ["@keyword.repeat"] = { bold = true },
+        ["@keyword.import"] = { bold = true },
+        ["@keyword.return"] = { bold = true },
       },
     })
 
